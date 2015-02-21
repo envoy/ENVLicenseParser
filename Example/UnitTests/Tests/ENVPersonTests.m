e62097288bcca06c959ea2535c0d29bc5a23161c
Keith Smiley <keithbsmiley@gmail.com>
2015-02-20 23:25:42 -0600

    Test isEqual:

--- Example/UnitTests/Tests/ENVPersonTests.m
+++ Example/UnitTests/Tests/ENVPersonTests.m
@@ -19,4 +19,33 @@
   XCTAssertFalse(person.expired);
 }
 
+- (void)testEquality
+{
+  NSString *address = [NSString stringWithFormat:AddressFormat, @"CA"];
+  ENVPerson *person1 = [[ENVPerson alloc] initWithName:Name
+                                             licenseID:LicenseNumber
+                                               address:address
+                                               expired:NO];
+  ENVPerson *person2 = [[ENVPerson alloc] initWithName:Name
+                                             licenseID:LicenseNumber
+                                               address:address
+                                               expired:NO];
+  XCTAssertEqualObjects(person1, person2);
+}
+
+- (void)testInequality
+{
+  NSString *address1 = [NSString stringWithFormat:AddressFormat, @"CA"];
+  ENVPerson *person1 = [[ENVPerson alloc] initWithName:Name
+                                             licenseID:LicenseNumber
+                                               address:address1
+                                               expired:NO];
+  NSString *address2 = [NSString stringWithFormat:AddressFormat, @"TX"];
+  ENVPerson *person2 = [[ENVPerson alloc] initWithName:Name
+                                             licenseID:LicenseNumber
+                                               address:address2
+                                               expired:NO];
+  XCTAssertNotEqualObjects(person1, person2);
+}
+
 @end
